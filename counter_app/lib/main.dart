d39f318ec9316f18a2589f591518509dd23f4919
Danilo Menezes <danilo.menezes@guide.com.br>
2022-07-20 22:40:22 -0300

    feat: criando contador

--- counter_app/lib/main.dart
+++ counter_app/lib/main.dart
@@ -1,5 +1,41 @@
+// ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
-void main(){
-  runApp(Container(color: Colors.purple));
+
+void main() {
+  runApp(MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.purple), home: HomePage()));
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var count = 0;
+
+  void increment() {
+    count++;
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Meu primeiro App"),
+        ),
+        body: Center(
+            child: Text(
+          "CONTADOR\n$count",
+          textAlign: TextAlign.center,
+        )),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () {
+            increment();
+          },
+        ));
+  }
 }
