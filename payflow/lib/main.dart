5d18875e690070afabe3cda4823c4441d3712e78
Danilo Menezes <danilo.menezes@guide.com.br>
2022-07-27 18:37:38 -0300

    feat: configurando Firebase

--- payflow/lib/main.dart
+++ payflow/lib/main.dart
@@ -1,21 +1,38 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:payflow/shared/themes/app_colors.dart';
-
-import 'modules/home/home_page.dart';
-import 'modules/login/login_page.dart';
-import 'modules/splash/splash_page.dart';
+import 'package:payflow/app_widget.dart';
 
 void main() {
-  runApp(AppWidget());
+  runApp(AppFirebase());
 }
 
-class AppWidget extends StatelessWidget {
+class AppFirebase extends StatefulWidget {
+  @override
+  State<AppFirebase> createState() => _AppFirebaseState();
+}
+
+class _AppFirebaseState extends State<AppFirebase> {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Pay Flor',
-      theme: ThemeData(primaryColor: AppColors.primary),
-      home: LoginPage(),
+    return FutureBuilder(
+      future: _initialization,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return const Material(
+            child: Center(
+                child: Text("Nao foi possivel configurar o firebase",
+                    textDirection: TextDirection.ltr)),
+          );
+        } else if (snapshot.connectionState == ConnectionState.done) {
+          return AppWidget();
+        } else {
+          return const Material(
+            child: Center(child: CircularProgressIndicator()),
+          );
+        }
+      },
     );
   }
 }
