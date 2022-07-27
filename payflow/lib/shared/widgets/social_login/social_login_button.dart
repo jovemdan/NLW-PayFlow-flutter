3d93dd7afbbd0375d99684a12176af77c0b6d6a8
Danilo Menezes <danilo.menezes@guide.com.br>
2022-07-27 18:40:29 -0300

    fix(adjusts): ajustando on tap

--- payflow/lib/shared/widgets/social_login/social_login_button.dart
+++ payflow/lib/shared/widgets/social_login/social_login_button.dart
@@ -11,6 +11,7 @@ class SocialLoginButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return InkWell(
+      onTap: onTap,
       child: Container(
         height: 56,
         decoration: BoxDecoration(
