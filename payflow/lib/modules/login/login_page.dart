b6f283fabe2c0ca67c66f9d43b25c3577c328e43
Danilo Menezes <danilo.menezes@guide.com.br>
2022-07-27 18:39:08 -0300

    feat: chamando controller  googleSignIn

--- payflow/lib/modules/login/login_page.dart
+++ payflow/lib/modules/login/login_page.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 import '../../shared/themes/app_colors.dart';
 import '../../shared/themes/app_images.dart';
 import '../../shared/themes/app_text_styles.dart';
 import '../../shared/widgets/social_login/social_login_button.dart';
+import 'login_controller.dart';
 
 class LoginPage extends StatefulWidget {
   LoginPage({Key? key}) : super(key: key);
@@ -13,6 +15,8 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final controller = LoginController();
+
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
@@ -53,7 +57,7 @@ class _LoginPageState extends State<LoginPage> {
                       padding:
                           const EdgeInsets.only(left: 40, right: 40, top: 40),
                       child: SocialLoginButton(onTap: () {
-                        print("Clicou");
+                        controller.googleSignIn(context);
                       }),
                     )
                   ],
