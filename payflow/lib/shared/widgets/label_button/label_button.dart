b01fd3e6ec1de34d39b7851abd96ab4fbfd725df
Danilo Menezes <danilo.menezes@guide.com.br>
2022-08-03 18:02:20 -0300

    feat: implementando LabelButton

--- payflow/lib/shared/widgets/label_button/label_button.dart
+++ payflow/lib/shared/widgets/label_button/label_button.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:payflow/shared/themes/app_text_styles.dart';
 
-class LabelButton extends StatefulWidget {
+class LabelButton extends StatelessWidget {
   final String label;
   final VoidCallback onPressed;
 
-  const LabelButton({Key? key, required this.label}) : super(key: key);
+  const LabelButton({Key? key, required this.label, required this.onPressed})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return TextButton(onPressed: onPressed, child: Text(label));
+    return Container(
+      height: 56,
+      child: TextButton(
+          onPressed: onPressed,
+          child: Text(label, style: TextStyles.buttonHeading)),
+    );
   }
 }
