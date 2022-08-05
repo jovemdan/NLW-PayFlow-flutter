77854fa9f9f31a1f9d84627854c009d4ac1b41ea
Danilo Menezes <danilo.menezes@guide.com.br>
2022-08-04 22:44:57 -0300

    feat: barcode_scanner incluindo bottomsheet

--- payflow/lib/modules/barcode_scanner/barcode_scanner_page.dart
+++ payflow/lib/modules/barcode_scanner/barcode_scanner_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_text_styles.dart';
+import 'package:payflow/shared/widgets/bottom_sheet/bottom_sheet_widget.dart';
 import 'package:payflow/shared/widgets/set_label_buttons/set_label_buttons.dart';
 
 class BarCodeScannerPage extends StatefulWidget {
@@ -13,6 +14,14 @@ class BarCodeScannerPage extends StatefulWidget {
 class _BarCodeScannerPageState extends State<BarCodeScannerPage> {
   @override
   Widget build(BuildContext context) {
+    return BottomSheetWidget(
+      title: "Não foi possível identificar um código de barras.",
+      subtitle: "Tente escanear novamente ou digite o código do seu boleto.",
+      primaryLabel: "Escanear novamente",
+      primaryOnPressed: () {},
+      secondaryLabel: "Digitar código",
+      secondaryOnPressed: () {},
+    );
     return SafeArea(
       top: true,
       bottom: true,
@@ -28,9 +37,9 @@ class _BarCodeScannerPageState extends State<BarCodeScannerPage> {
                 centerTitle: true,
                 leading: BackButton(color: AppColors.background)),
             body: Column(children: [
-              Expanded(child: Container(color: Colors.black)),
+              Expanded(child: Container(color: Colors.black.withOpacity(0.6))),
               Expanded(flex: 2, child: Container(color: Colors.transparent)),
-              Expanded(child: Container(color: Colors.black)),
+              Expanded(child: Container(color: Colors.black.withOpacity(0.6))),
             ]),
             bottomNavigationBar: SetLabelButtons(
                 primaryLabel: "Inserir código do boleto",
